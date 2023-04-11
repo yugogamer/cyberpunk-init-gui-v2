f0a2ada84cce9841583588a51dde5f8d0602297c
yugogamer <57806524+yugogamer@users.noreply.github.com>
2023-04-11 21:37:07 +0200

    update icon

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,7 @@
   <section class="notification primary is-dark">
     <div class="columns">
       <div class="column is-1">
-        <img
-          src="https://media.discordapp.net/attachments/970331977263775785/1075376704203264010/SPOILER_FB_IMG_1676460064630.jpg?width=1056&height=671"
-          alt="logo"
-        />
+        <img src="/assets/logo.png" alt="logo" />
       </div>
       <div class="column">
         <h1 class="title">Cyberpunk Red</h1>
